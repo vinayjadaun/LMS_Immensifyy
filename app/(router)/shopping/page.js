d69a2ec7b0deaf40535ff5f1ef73e7d59ef447b7
vinay_jadaun <vinay11112003@gmail.com>
2024-05-15 22:16:38 +0530

    finish

--- app/(router)/shopping/page.js
+++ app/(router)/shopping/page.js
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const shop = () => {
   return (
-    <div>shop</div>
+    <div className='grid sm:grid-cols-4 grid-cols-1 bg-white p-5 gap-1'>
+    {/* left section */}
+     <div className='col-span-2'>
+     <Image src='/panda1_prev_ui.png' alt="any" height={600} width={600}/>
+     </div>
+     {/* right section */}
+     
+     <div className='col-span-2 p-5  text-center items-center bg-white rounded-xl'>
+     <h2 className='font-bold sm:mr-[100px] mr-0 text-primary sm:mt-[150px] mt-0 sm:text-[70px] text-[50px]'>Comming Soon</h2>
+     </div>
+   </div>
   )
 }
 
