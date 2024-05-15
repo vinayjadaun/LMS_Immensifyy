acf1c217f746df626f785dde218fe9e08172b232
vinay_jadaun <vinay11112003@gmail.com>
2024-05-15 17:10:02 +0530

    @15-05-2024

--- app/(router)/cource-preview/[courceId]/_components/CourceVideoDescription.js
+++ app/(router)/cource-preview/[courceId]/_components/CourceVideoDescription.js
@@ -12,7 +12,7 @@ const CourceVideoDescription = ({courceinfo,activeChapter,watchMode=false,setCha
         <h2 className='text-[20px] font-semibold'>{courceinfo.name}</h2>
         <h2 className='text-gray-500 text-[14px]'>{courceinfo.auther}</h2>
         <VideoPlayer videourl={courceinfo?.chapter[activeChapter]?.video?.url} banner={!watchMode?courceinfo.banner.url:null}/>
-        <h2 className='mt-5 text-[17px] font-semibold'>{watchMode?<span className='flex justify-between items-center'>{courceinfo?.chapter[activeChapter]?.name}
+        <h2 className='mt-5 text-[17px] font-semibold'>{watchMode?<span className='flex sm:flex-row flex-col justify-between items-center'>{courceinfo?.chapter[activeChapter]?.name}
         <Button className='text-white' onClick={()=>setChapterCompleted(courceinfo?.chapter[activeChapter]?.id)}>Mark Completed</Button>
         </span>:<span>About This Cource</span>}</h2>
         <div className='overflow-hidden'>
