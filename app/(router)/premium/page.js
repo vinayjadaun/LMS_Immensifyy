024202e23d0d1d948111e633448948bef9e8eddf
vinay_jadaun <vinay11112003@gmail.com>
2024-05-17 19:01:43 +0530

    222

--- app/(router)/premium/page.js
+++ app/(router)/premium/page.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { useContext, useEffect } from 'react'
 
-const page = () => {
+const Page = () => {
   const router = useRouter();
   const{membershipid,SetMembershipid}=useContext(Membership);
   const{Ismember,SetIsmember}=useContext(IsmemberContext);
@@ -458,4 +458,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
