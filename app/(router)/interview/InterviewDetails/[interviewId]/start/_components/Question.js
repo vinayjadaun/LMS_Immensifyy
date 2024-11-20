579f4faabce1abe998f128d82b37a4453cf3c1da
vinay_jadaun <vinay11112003@gmail.com>
2024-11-20 16:06:15 +0530

    done

--- app/(router)/interview/InterviewDetails/[interviewId]/start/_components/Question.js
+++ app/(router)/interview/InterviewDetails/[interviewId]/start/_components/Question.js
@@ -1,8 +1,11 @@
 import { Lightbulb, Volume2Icon } from 'lucide-react';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Question = ({mockinterviewquestion,activeindex,setactiveindex}) => {
-  
+     useEffect(()=>{
+      console.log("This is the set of mock question: ")
+      console.log(mockinterviewquestion);
+     },[mockinterviewquestion]);
     const texttospeech=(text)=>{
       if('speechSynthesis' in window){
         const speech=new SpeechSynthesisUtterance(text);
@@ -14,12 +17,12 @@ const Question = ({mockinterviewquestion,activeindex,setactiveindex}) => {
   return (
     <div className='p-5 border-[2px] rounded-lg'>
         <div className='grid grid-cols-2 md:grid-cols-3 mb-5 lg:grid-cols-4 gap-5'>
-            {Array.isArray(mockinterviewquestion) && mockinterviewquestion.map((Ques,index)=>(
+            {mockinterviewquestion.questions_and_answers?.map((Ques,index)=>(
               <h2 key={index} onClick={()=>setactiveindex(index)} className={`p-2 rounded-[20px] text-xs md:text-sm text-center cursor-pointer ${activeindex==index?'bg-primary text-white':'bg-gray-300 text-black'}`}>Question #{index+1}</h2>
            ) )}
         </div>
-        <h2 className='my-7 text-sm md:text-md'>Q.{activeindex+1} : {mockinterviewquestion[activeindex]?.question}</h2>
-        <Volume2Icon className='cursor-pointer' onClick={()=>texttospeech(mockinterviewquestion[activeindex]?.question)}/>
+        <h2 className='my-7 text-sm md:text-md'>Q.{activeindex+1} : {mockinterviewquestion.questions_and_answers[activeindex]?.question}</h2>
+        <Volume2Icon className='cursor-pointer' onClick={()=>texttospeech(mockinterviewquestion.questions_and_answers[activeindex]?.question)}/>
         <div className='border rounded-lg p-5 bg-blue-100 mt-5'>
           <h2 className='flex gap-2 items-center text-blue-700'>
           <Lightbulb/>
